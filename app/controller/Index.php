a5e098e67eabdf528b87331ed360e75fb2e0ce6f
panjie <3792535@qq.com>
2021-01-12 11:33:57 +0800

    数据库初始化

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,11 +3,16 @@ namespace app\controller;
 
 use app\BaseController;
 
+// 找住在think镇facade村的Db来帮个忙
+use think\facade\Db;
+
 class Index extends BaseController
 {
     public function index()
     {
-        return '<h1 style="color:red">Hello World!</h1>';
+        // Db提供了一个query方法，调用该方法可以进行数据查询
+        // 使用select * from yunzhi_teacher查询数据库，并输出查询结果
+        dump(Db::query('select * from yunzhi_teacher'));
     }
 
     public function hello($name = 'ThinkPHP6')
