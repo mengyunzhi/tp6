fc0c64bbf5c5d2882b1e52249f3ce3122ecbb664
panjie <3792535@qq.com>
2021-01-13 09:28:04 +0800

    2.2.3

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -5,15 +5,21 @@ use app\BaseController;
 
 class Index extends BaseController
 {
-    public function index()
-    {
-        // think镇facade村的Db提供了一个query方法，调用该方法可以进行数据查询
-        // 使用select * from yunzhi_teacher查询数据库
-        $teachers = \think\facade\Db::query('select * from yunzhi_teacher');
-        
-        // 调用tp提供的dump方法，友好的输出查询结果
-        dump($teachers);
-    }
+        public function index()
+        {
+            // think镇facade村的Db提供了一个query方法，调用该方法可以进行数据查询
+            // 使用select * from yunzhi_teacher查询数据库
+            $teachers = \think\facade\Db::query('select * from yunzhi_teacher');
+            
+            // 调用tp提供的dump方法，友好的输出查询结果
+            dump($teachers);
+
+            dump($teachers[0]);
+
+            dump($teachers[0]['name']);
+
+            echo $teachers[0]['name'];
+        }
 
     public function hello($name = 'ThinkPHP6')
     {
