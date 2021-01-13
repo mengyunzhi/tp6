ecaa8253151d7189e41d28d4208ad7a1cd1025ea
panjie <3792535@qq.com>
2021-01-13 08:47:02 +0800

    update test

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -3,16 +3,13 @@ namespace app\controller;
 
 use app\BaseController;
 
-// 找住在think镇facade村的Db来帮个忙
-use think\facade\Db;
-
 class Index extends BaseController
 {
     public function index()
     {
-        // Db提供了一个query方法，调用该方法可以进行数据查询
+        // think镇facade村的Db提供了一个query方法，调用该方法可以进行数据查询
         // 使用select * from yunzhi_teacher查询数据库，并输出查询结果
-        dump(Db::query('select * from yunzhi_teacher'));
+        dump(\think\facade\Db::query('select * from yunzhi_teacher'));
     }
 
     public function hello($name = 'ThinkPHP6')
