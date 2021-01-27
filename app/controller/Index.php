03c52e5d7f2ff2f6561a2ebead57f9108e8e83a2
panjie <3792535@qq.com>
2021-01-27 12:00:12 +0800

    step2.2.4

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -5,21 +5,15 @@ use app\BaseController;
 
 class Index extends BaseController
 {
-        public function index()
-        {
-            // think镇facade村的Db提供了一个query方法，调用该方法可以进行数据查询
-            // 使用select * from yunzhi_teacher查询数据库
-            $teachers = \think\facade\Db::query('select * from yunzhi_teacher');
-            
-            // 调用tp提供的dump方法，友好的输出查询结果
-            dump($teachers);
+    public function index()
+    {
+        // 找app\model命名空间的Teacher来帮忙
+        $teacher = new \app\model\Teacher();
 
-            dump($teachers[0]);
-
-            dump($teachers[0]['name']);
-
-            echo $teachers[0]['name'];
-        }
+        // 获取teacher表中的所有数据
+        $teachers = $teacher->select();
+        dump($teachers);
+    }
 
     public function hello($name = 'ThinkPHP6')
     {
---
+++ app/model/Teacher.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\model;
+
+class Teacher extends \Think\Model
+{
+
+}
